72036581c199cfed1554b7aa2c3f840ac16da18a
Reymond Pamelar <rpamelar@sdsu.edu>
2020-11-29 22:24:25 -0800

    Added Main Character Model with top-down Movement

---
+++ Assets/Artwork/MainCharacter/Scripts/InputHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public Vector2 InputVector { get; private set; }
+
+    public Vector3 MousePosition { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+        InputVector = new Vector2(h, v);
+
+        MousePosition = Input.mousePosition;
+    }
+}
---
+++ Assets/Artwork/MainCharacter/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject cam;
+
+    private InputHandler _input;
+
+    [SerializeField]
+    private float moveSpeed;
+
+    [SerializeField]
+    private float rotateSpeed;
+
+    private void Awake()
+    {
+        _input = GetComponent<InputHandler>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        Ray r = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitDist = 0.0f;
+
+        if(playerPlane.Raycast(r, out hitDist))
+        {
+            Vector3 targetPoint = r.GetPoint(hitDist);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+            targetRotation.x = 0;
+            targetRotation.z = 0;
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 7f * Time.deltaTime);
+        }
+
+        var targetVector = new Vector3(_input.InputVector.x, 0, _input.InputVector.y);
+
+        //move
+        MoveTowardTarget(targetVector);
+    }
+
+    private void MoveTowardTarget(Vector3 targetVector)
+    {
+        var speed = moveSpeed * Time.deltaTime;
+        transform.Translate(targetVector * speed);
+
+    }
+}
