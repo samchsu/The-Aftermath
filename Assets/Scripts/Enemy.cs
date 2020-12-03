f7c684d04422f43c8608f50bec33f55af02da481
Reian Festejo <rfestejo@sdsu.edu>
2020-12-03 01:19:51 -0800

    Created first enemy that follows and moves wherever the player is

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody myRB;
+    public float moveSpeed;
+
+    public Player thePlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody>();
+        thePlayer = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(thePlayer.transform.position);
+    }
+
+    void FixedUpdate()
+    {
+        myRB.velocity = (transform.forward * moveSpeed);
+    }
+}
