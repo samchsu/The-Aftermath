5b601f716f417f73151501670d68906f01a60c37
Reymond Pamelar <rpamelar@sdsu.edu>
2020-12-08 18:02:16 -0800

    Added kill count and hitbox detection

--- Assets/Artwork/MainCharacter/Scripts/Player.cs
+++ Assets/Artwork/MainCharacter/Scripts/Player.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
     public float playerHealth;
     public barSlideHandler b;
 
+    public int playerKills;
+    public TMP_Text killText;
+
     // Start is called before the first frame update
     void Start()
     {
         b.SetMaxCD(playerHealth);
+
+        playerKills = 0;
     }
 
     // Update is called once per frame
@@ -18,6 +24,11 @@ public class Player : MonoBehaviour
     {
         b.SetCD(playerHealth);
 
+        killText.text = playerKills.ToString();
     }
 
+    public void addKill()
+    {
+        playerKills += 1;
+    }
 }
