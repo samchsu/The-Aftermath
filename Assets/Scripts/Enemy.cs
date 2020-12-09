2d07e89f0a85a4717b23b14b4973c03cd3a19652
Reian Festejo <rfestejo@sdsu.edu>
2020-12-08 23:37:46 -0800

    Added one enemy animation so they can be idle, walk, and attack player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,25 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    public Animator anim;
     private Rigidbody myRB;
     public float moveSpeed;
-
-    public Player thePlayer;
+    public NavMeshAgent enemy;
+    public Player player;
 
     // Start is called before the first frame update
     void Start()
     {
         myRB = GetComponent<Rigidbody>();
-        thePlayer = FindObjectOfType<Player>();
+        player = FindObjectOfType<Player>();
+        enemy = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(thePlayer.transform.position);
+        if (enemy.stoppingDistance < 5f)
+        {
+            anim.SetTrigger("Attack");
+        }
+        if (enemy.destination == player.transform.position)
+        {
+            anim.SetBool("isWalking", false);
+        }
+        else
+        {
+            anim.SetBool("isWalking", true);
+            transform.LookAt(player.transform.position);
+            //enemy.destination = player.transform.position;
+        }
     }
 
     void FixedUpdate()
